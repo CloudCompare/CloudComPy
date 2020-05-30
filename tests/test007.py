04d8797030dded440b0f4e9d62fc37f9dfcb248f
Paul RASCLE <paul.rascle@openfields.fr>
2020-05-30 17:34:56 +0200

    add function and test crop2D

---
+++ tests/test007.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+##########################################################################
+#                                                                        #
+#                                PYCC                                    #
+#                                                                        #
+#  This program is free software; you can redistribute it and/or modify  #
+#  it under the terms of the GNU Library General Public License as       #
+#  published by the Free Software Foundation; version 2 or later of the  #
+#  License.                                                              #
+#                                                                        #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+#  GNU General Public License for more details.                          #
+#                                                                        #
+#          Copyright 2020 Paul RASCLE www.openfields.fr                  #
+#                                                                        #
+##########################################################################
+
+import os
+import sys
+import math
+from gendata import getSampleCloud, getSamplePoly, dataDir, isCoordEqual
+from PyQt5.QtWidgets import QApplication
+import cloudCompare as cc
+
+app = QApplication(sys.argv)
+cc.initCC.init()  # to do once before using plugins or dealing with numpy
+
+cloud = cc.loadPointCloud(getSampleCloud(2.0))
+poly = cc.loadPolyline(getSamplePoly("poly1"))
+
+cloudCropZ = cloud.crop2D(poly, 2, True)
+cc.SavePointCloud(cloudCropZ, os.path.join(dataDir, "cloudCropZ.xyz"))
+npts = cloudCropZ.size()
+print("cloud.size %s" % npts)
+if npts != 189981:
+    raise RuntimeError
+
+cloudCropX = cloud.crop2D(poly, 1, True)
+cc.SavePointCloud(cloudCropX, os.path.join(dataDir, "cloudCropX.xyz"))
+npts = cloudCropX.size()
+print("cloud.size %s" % npts)
+if npts != 458593:
+    raise RuntimeError
+
+cloudCropY = cloud.crop2D(poly, 0, True)
+cc.SavePointCloud(cloudCropY, os.path.join(dataDir, "cloudCropY.xyz"))
+npts = cloudCropY.size()
+print("cloud.size %s" % npts)
+if npts != 399968:
+    raise RuntimeError
