93131eb82f5bb6cc38fbfa501a538d38e93a3471
Paul RASCLE <paul.rascle@openfields.fr>
2021-03-25 18:21:13 +0100

    first automatic converter QString, working

--- boostPython/cloudComPy.cpp
+++ boostPython/cloudComPy.cpp
@@ -17,19 +17,92 @@
 //##########################################################################
 
 #include <boost/python.hpp>
+//#include "converters.hpp"
 
 #include "initCC.h"
 #include "pyCC.h"
 #include "PyScalarType.h"
+#include <QString>
+
+#include "ccTrace.h"
 
 char const* greet()
 {
    return "hello, world";
 }
 
+//using namespace boost::python;
+
+namespace
+{
+
+struct QString_to_python_str
+{
+    static PyObject* convert(const QString& s)
+    {
+        return boost::python::incref(boost::python::object(s.toUtf8().constData()).ptr());
+    }
+};
+
+struct QString_from_python_str
+{
+    QString_from_python_str()
+    {
+        CCTRACE("register QString_from_python_str");
+        boost::python::converter::registry::push_back(&convertible, &construct, boost::python::type_id<QString>());
+    }
+
+    // Determine if obj_ptr can be converted in a QString
+    static void* convertible(PyObject* obj_ptr)
+    {
+        CCTRACE("convertible to QString?");
+        if (!PyUnicode_Check(obj_ptr))
+            return 0;
+        return obj_ptr;
+    }
+
+    // Convert obj_ptr into a QString
+    static void construct(PyObject* obj_ptr, boost::python::converter::rvalue_from_python_stage1_data* data)
+    {
+        CCTRACE("construct");
+        // Extract the character data from the python string
+        const char* value = PyUnicode_AsUTF8(obj_ptr);
+
+        // Verify that obj_ptr is a string (should be ensured by convertible())
+        if (value == 0) boost::python::throw_error_already_set();
+
+        // Grab pointer to memory into which to construct the new QString
+        void* storage = ((boost::python::converter::rvalue_from_python_storage<QString>*) data)->storage.bytes;
+
+        // in-place construct the new QString using the character data
+        // extracted from the Python object
+        new (storage) QString(value);
+
+        // Stash the memory chunk pointer for later use by boost.python
+        data->convertible = storage;
+    }
+};
+
+void initializeConverters()
+{
+    using namespace boost::python;
+
+    // register the to-python converter
+    CCTRACE("initializeConverters");
+    to_python_converter<QString, QString_to_python_str, false>(); //"false" because QString_to_python_str has no member get_pytype
+
+    // register the from-python converter
+    QString_from_python_str();
+}
+
+} //namespace anonymous
+
 BOOST_PYTHON_MODULE(cloudComPy)
 {
     using namespace boost::python;
+
+    initializeConverters();
+
     def("greet", greet);
 
     enum_<CC_SHIFT_MODE>("CC_SHIFT_MODE")
@@ -37,12 +110,47 @@ BOOST_PYTHON_MODULE(cloudComPy)
         .value("XYZ", XYZ)
         ;
 
-    class_<ccPointCloud>("ccPointCloud")
+    enum_<CC_FILE_ERROR>("CC_FILE_ERROR")
+        .value("CC_FERR_NO_ERROR", CC_FERR_NO_ERROR)
+        .value("CC_FERR_BAD_ARGUMENT", CC_FERR_BAD_ARGUMENT)
+        .value("CC_FERR_UNKNOWN_FILE", CC_FERR_UNKNOWN_FILE)
+        .value("CC_FERR_WRONG_FILE_TYPE", CC_FERR_WRONG_FILE_TYPE)
+        .value("CC_FERR_WRITING", CC_FERR_WRITING)
+        .value("CC_FERR_READING", CC_FERR_READING)
+        .value("CC_FERR_NO_SAVE", CC_FERR_NO_SAVE)
+        .value("CC_FERR_NO_LOAD", CC_FERR_NO_LOAD)
+        .value("CC_FERR_BAD_ENTITY_TYPE", CC_FERR_BAD_ENTITY_TYPE)
+        .value("CC_FERR_CANCELED_BY_USER", CC_FERR_CANCELED_BY_USER)
+        .value("CC_FERR_NOT_ENOUGH_MEMORY", CC_FERR_NOT_ENOUGH_MEMORY)
+        .value("CC_FERR_MALFORMED_FILE", CC_FERR_MALFORMED_FILE)
+        .value("CC_FERR_CONSOLE_ERROR", CC_FERR_CONSOLE_ERROR)
+        .value("CC_FERR_BROKEN_DEPENDENCY_ERROR", CC_FERR_BROKEN_DEPENDENCY_ERROR)
+        .value("CC_FERR_FILE_WAS_WRITTEN_BY_UNKNOWN_PLUGIN", CC_FERR_FILE_WAS_WRITTEN_BY_UNKNOWN_PLUGIN)
+        .value("CC_FERR_THIRD_PARTY_LIB_FAILURE", CC_FERR_THIRD_PARTY_LIB_FAILURE)
+        .value("CC_FERR_THIRD_PARTY_LIB_EXCEPTION", CC_FERR_THIRD_PARTY_LIB_EXCEPTION)
+        .value("CC_FERR_NOT_IMPLEMENTED", CC_FERR_NOT_IMPLEMENTED)
+        ;
+
+    enum_<CurvatureType>("CurvatureType")
+        .value("GAUSSIAN_CURV", GAUSSIAN_CURV)
+        .value("MEAN_CURV", MEAN_CURV)
+        .value("NORMAL_CHANGE_RATE", NORMAL_CHANGE_RATE)
+        ;
+
+    class_<ccPointCloud>("ccPointCloud", no_init)
         .def("hasScalarFields", &ccPointCloud::hasScalarFields);
+
+    class_<ccPolyline>("ccPolyline", no_init)
+        .def("is2DMode", &ccPolyline::is2DMode);
 
     //ccPointCloud* (*l0)(const char*) = &loadPointCloud;
     def("loadPointCloud", loadPointCloud,
         return_value_policy<reference_existing_object>());
+
+    def("loadPolyline", loadPolyline,
+        return_value_policy<reference_existing_object>());
+
+    def("SavePointCloud", SavePointCloud);
 
 
 }
---
+++ boostPython/converters.hpp
@@ -0,0 +1,135 @@
+//##########################################################################
+//#                                                                        #
+//#                                PYCC                                    #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU Library General Public License as       #
+//#  published by the Free Software Foundation; version 2 or later of the  #
+//#  License.                                                              #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          Copyright 2021 Paul RASCLE www.openfields.fr                  #
+//#                                                                        #
+//##########################################################################
+
+// original code from:
+// https://misspent.wordpress.com/2009/09/27/how-to-write-boost-python-converters/
+
+#ifndef CONVERTERS_HPP_
+#define CONVERTERS_HPP_
+
+#include <boost/python.hpp>
+#include <Python.h>
+#include <QString>
+
+using namespace boost::python;
+
+struct QString_to_python_str
+{
+    static PyObject* convert(QString const& s)
+      {
+        return boost::python::incref(
+          boost::python::object(
+            s.toUtf8().constData()).ptr());
+      }
+};
+
+BOOST_PYTHON_MODULE(to_python_converter)
+{
+    using namespace boost::python;
+
+    to_python_converter<QString, QString_to_python_str, false>(); //"false" because QString_to_python_str has no member get_pytype
+}
+
+//#include <boost/python/reference.hpp>
+//#include <boost/python/module.hpp>
+//struct tag {};
+//tag make_tag() { return tag(); }
+//
+//struct tag_to_noddy
+//{
+//    static PyObject* convert(tag const& x)
+//    {
+//        return PyObject_New(noddy_NoddyObject, &noddy_NoddyType);
+//    }
+//    static PyTypeObject const* get_pytype()
+//    {
+//        return &noddy_NoddyType;
+//    }
+//};
+//
+//#include <boost/python.hpp>
+//#include <Python.h>
+//#include <QString>
+//
+//struct QString_to_python_str
+//{
+//    static PyObject* convert(QString const& s)
+//      {
+//        return boost::python::incref(
+//          boost::python::object(
+//            s.toUtf8().constData()).ptr());
+//      }
+//};
+//
+//struct QString_from_python_str
+//{
+//    QString_from_python_str()
+//    {
+//      boost::python::converter::registry::push_back(
+//        &convertible,
+//        &construct,
+//        boost::python::type_id<QString>());
+//    }
+//
+//    // Determine if obj_ptr can be converted in a QString
+//    static void* convertible(PyObject* obj_ptr)
+//    {
+//      if (!PyUnicode_Check(obj_ptr)) return 0;
+//      return obj_ptr;
+//    }
+//
+//    // Convert obj_ptr into a QString
+//    static void construct(
+//    PyObject* obj_ptr,
+//    boost::python::converter::rvalue_from_python_stage1_data* data)
+//    {
+//      // Extract the character data from the python string
+//      const char* value = PyUnicode_AsUTF8(obj_ptr);
+//
+//      // Verify that obj_ptr is a string (should be ensured by convertible())
+//      assert(value);
+//
+//      // Grab pointer to memory into which to construct the new QString
+//      void* storage = (
+//        (boost::python::converter::rvalue_from_python_storage<QString>*)
+//        data)->storage.bytes;
+//
+//      // in-place construct the new QString using the character data
+//      // extraced from the python object
+//      new (storage) QString(value);
+//
+//      // Stash the memory chunk pointer for later use by boost.python
+//      data->convertible = storage;
+//    }
+//};
+//
+//void initializeConverters()
+//{
+//  using namespace boost::python;
+//
+//  // register the to-python converter
+//  to_python_converter<
+//    QString,
+//    QString_to_python_str>();
+//
+//  // register the from-python converter
+//  QString_from_python_str();
+//}
+
+#endif /* CONVERTERS_HPP_ */
+
