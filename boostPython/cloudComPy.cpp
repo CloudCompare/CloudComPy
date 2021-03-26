1f242720f40802ba3ac91fe47b8921e59615259d
Paul RASCLE <paul.rascle@openfields.fr>
2021-03-26 13:46:59 +0100

    converters in a separate file

--- boostPython/cloudComPy.cpp
+++ boostPython/cloudComPy.cpp
@@ -17,7 +17,7 @@
 //##########################################################################
 
 #include <boost/python.hpp>
-//#include "converters.hpp"
+#include "converters.hpp"
 
 #include "initCC.h"
 #include "pyCC.h"
@@ -30,72 +30,6 @@ char const* greet()
 {
    return "hello, world";
 }
-
-//using namespace boost::python;
-
-namespace
-{
-
-struct QString_to_python_str
-{
-    static PyObject* convert(const QString& s)
-    {
-        return boost::python::incref(boost::python::object(s.toUtf8().constData()).ptr());
-    }
-};
-
-struct QString_from_python_str
-{
-    QString_from_python_str()
-    {
-        CCTRACE("register QString_from_python_str");
-        boost::python::converter::registry::push_back(&convertible, &construct, boost::python::type_id<QString>());
-    }
-
-    // Determine if obj_ptr can be converted in a QString
-    static void* convertible(PyObject* obj_ptr)
-    {
-        CCTRACE("convertible to QString?");
-        if (!PyUnicode_Check(obj_ptr))
-            return 0;
-        return obj_ptr;
-    }
-
-    // Convert obj_ptr into a QString
-    static void construct(PyObject* obj_ptr, boost::python::converter::rvalue_from_python_stage1_data* data)
-    {
-        CCTRACE("construct");
-        // Extract the character data from the python string
-        const char* value = PyUnicode_AsUTF8(obj_ptr);
-
-        // Verify that obj_ptr is a string (should be ensured by convertible())
-        if (value == 0) boost::python::throw_error_already_set();
-
-        // Grab pointer to memory into which to construct the new QString
-        void* storage = ((boost::python::converter::rvalue_from_python_storage<QString>*) data)->storage.bytes;
-
-        // in-place construct the new QString using the character data
-        // extracted from the Python object
-        new (storage) QString(value);
-
-        // Stash the memory chunk pointer for later use by boost.python
-        data->convertible = storage;
-    }
-};
-
-void initializeConverters()
-{
-    using namespace boost::python;
-
-    // register the to-python converter
-    CCTRACE("initializeConverters");
-    to_python_converter<QString, QString_to_python_str, false>(); //"false" because QString_to_python_str has no member get_pytype
-
-    // register the from-python converter
-    QString_from_python_str();
-}
-
-} //namespace anonymous
 
 BOOST_PYTHON_MODULE(cloudComPy)
 {
--- boostPython/converters.hpp
+++ boostPython/converters.hpp
@@ -26,110 +26,71 @@
 #include <Python.h>
 #include <QString>
 
-using namespace boost::python;
+#include "pyccTrace.h"
+
+namespace
+{
 
 struct QString_to_python_str
 {
-    static PyObject* convert(QString const& s)
-      {
-        return boost::python::incref(
-          boost::python::object(
-            s.toUtf8().constData()).ptr());
-      }
+    static PyObject* convert(const QString& s)
+    {
+        return boost::python::incref(boost::python::object(s.toUtf8().constData()).ptr());
+    }
 };
 
-BOOST_PYTHON_MODULE(to_python_converter)
+struct QString_from_python_str
+{
+    QString_from_python_str()
+    {
+        CCTRACE("register QString_from_python_str");
+        boost::python::converter::registry::push_back(&convertible, &construct, boost::python::type_id<QString>());
+    }
+
+    // Determine if obj_ptr can be converted in a QString
+    static void* convertible(PyObject* obj_ptr)
+    {
+        CCTRACE("convertible to QString?");
+        if (!PyUnicode_Check(obj_ptr))
+            return 0;
+        return obj_ptr;
+    }
+
+    // Convert obj_ptr into a QString
+    static void construct(PyObject* obj_ptr, boost::python::converter::rvalue_from_python_stage1_data* data)
+    {
+        CCTRACE("construct");
+        // Extract the character data from the python string
+        const char* value = PyUnicode_AsUTF8(obj_ptr);
+
+        // Verify that obj_ptr is a string (should be ensured by convertible())
+        if (value == 0) boost::python::throw_error_already_set();
+
+        // Grab pointer to memory into which to construct the new QString
+        void* storage = ((boost::python::converter::rvalue_from_python_storage<QString>*) data)->storage.bytes;
+
+        // in-place construct the new QString using the character data
+        // extracted from the Python object
+        new (storage) QString(value);
+
+        // Stash the memory chunk pointer for later use by boost.python
+        data->convertible = storage;
+    }
+};
+
+void initializeConverters()
 {
     using namespace boost::python;
 
+    // register the to-python converter
+    CCTRACE("initializeConverters");
     to_python_converter<QString, QString_to_python_str, false>(); //"false" because QString_to_python_str has no member get_pytype
+
+    // register the from-python converter
+    QString_from_python_str();
 }
 
-//#include <boost/python/reference.hpp>
-//#include <boost/python/module.hpp>
-//struct tag {};
-//tag make_tag() { return tag(); }
-//
-//struct tag_to_noddy
-//{
-//    static PyObject* convert(tag const& x)
-//    {
-//        return PyObject_New(noddy_NoddyObject, &noddy_NoddyType);
-//    }
-//    static PyTypeObject const* get_pytype()
-//    {
-//        return &noddy_NoddyType;
-//    }
-//};
-//
-//#include <boost/python.hpp>
-//#include <Python.h>
-//#include <QString>
-//
-//struct QString_to_python_str
-//{
-//    static PyObject* convert(QString const& s)
-//      {
-//        return boost::python::incref(
-//          boost::python::object(
-//            s.toUtf8().constData()).ptr());
-//      }
-//};
-//
-//struct QString_from_python_str
-//{
-//    QString_from_python_str()
-//    {
-//      boost::python::converter::registry::push_back(
-//        &convertible,
-//        &construct,
-//        boost::python::type_id<QString>());
-//    }
-//
-//    // Determine if obj_ptr can be converted in a QString
-//    static void* convertible(PyObject* obj_ptr)
-//    {
-//      if (!PyUnicode_Check(obj_ptr)) return 0;
-//      return obj_ptr;
-//    }
-//
-//    // Convert obj_ptr into a QString
-//    static void construct(
-//    PyObject* obj_ptr,
-//    boost::python::converter::rvalue_from_python_stage1_data* data)
-//    {
-//      // Extract the character data from the python string
-//      const char* value = PyUnicode_AsUTF8(obj_ptr);
-//
-//      // Verify that obj_ptr is a string (should be ensured by convertible())
-//      assert(value);
-//
-//      // Grab pointer to memory into which to construct the new QString
-//      void* storage = (
-//        (boost::python::converter::rvalue_from_python_storage<QString>*)
-//        data)->storage.bytes;
-//
-//      // in-place construct the new QString using the character data
-//      // extraced from the python object
-//      new (storage) QString(value);
-//
-//      // Stash the memory chunk pointer for later use by boost.python
-//      data->convertible = storage;
-//    }
-//};
-//
-//void initializeConverters()
-//{
-//  using namespace boost::python;
-//
-//  // register the to-python converter
-//  to_python_converter<
-//    QString,
-//    QString_to_python_str>();
-//
-//  // register the from-python converter
-//  QString_from_python_str();
-//}
+} //namespace anonymous
 
 #endif /* CONVERTERS_HPP_ */
 
