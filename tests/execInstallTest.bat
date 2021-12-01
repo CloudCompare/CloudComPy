d599dfe6742e413c70e7ef4180ce4014140e17ab
Paul Rascle <paul.rascle@openfields.fr>
2021-12-01 16:02:17 +0100

    windows porting

---
+++ tests/execInstallTest.bat
@@ -0,0 +1,15 @@
+@echo off
+@set PREVPATH=%PATH%
+@set PREVPYTHONPATH=%PYTHONPATH%
+@set SCRIPT_DIR=%~dp0
+@set CLOUDCOMPY_ROOT=%SCRIPT_DIR%..\..
+@set QT_PLUGIN_PATH=%CLOUDCOMPY_ROOT%\qtplugins
+@set PYTHONPATH=%CLOUDCOMPY_ROOT%/CloudCompare;%PYTHONPATH%
+@set PYTHONPATH=%SCRIPT_DIR%;%PYTHONPATH%
+@set PATH=.;%CLOUDCOMPY_ROOT%/CloudCompare;%CLOUDCOMPY_ROOT%/ccViewer;%SCRIPT_DIR%;%PATH%
+python %1
+set PythonError=%errorlevel%
+echo "Python exit code = %PythonError%"
+@set PYTHONPATH=%PREVPYTHONPATH%
+@set PATH=%PREVPATH%
+exit %PythonError%
