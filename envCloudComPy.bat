701c36ce82d50ae38f06ab2ca907cd7a86b09699
Paul RASCLE <paul.rascle@openfields.fr>
2022-09-16 19:59:17 +0200

    trying to use Qt5.15 from conda on Windows: requires to remove translations...

--- envCloudComPy.bat
+++ envCloudComPy.bat
@@ -3,7 +3,6 @@ rem this script sets the environment for CloudComPy. The Conda environment must 
 @echo off
 @set SCRIPT_DIR=%~dp0
 @set CLOUDCOMPY_ROOT=%SCRIPT_DIR%
-@set QT_PLUGIN_PATH=%CLOUDCOMPY_ROOT%\qtplugins
 @set PYTHONPATH=%CLOUDCOMPY_ROOT%\CloudCompare;%PYTHONPATH%
 @set PYTHONPATH=%CLOUDCOMPY_ROOT%\doc\PythonAPI_test;%PYTHONPATH%
 @set PATH=%CLOUDCOMPY_ROOT%\CloudCompare;%CLOUDCOMPY_ROOT%\ccViewer;%SCRIPT_DIR%;%PATH%
